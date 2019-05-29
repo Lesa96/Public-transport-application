b6a20a39988c022f5106877d5ff3b10350e3f026
Stefan Milenković <milenko2204@gmail.com>
2019-05-29 13:58:27 +0200

    Model changes
    Added PassengerType field to ApplicationUser

--- WebApp/WebApp/Models/ApplicationUser.cs
+++ WebApp/WebApp/Models/ApplicationUser.cs
@@ -8,12 +8,14 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Web;
+using WebApp.Models.Enums;
 
 namespace WebApp.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public PassengerType PassengerType { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
