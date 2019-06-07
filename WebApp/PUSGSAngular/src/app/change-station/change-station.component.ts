9e23dfdefab87a430a1cf7d41e43231176e16f96
Sa�a Dragomirovi? <E375-2015@ftn.ns>
2019-06-07 18:03:31 +0200

    Added EditStation

---
+++ WebApp/PUSGSAngular/src/app/change-station/change-station.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-station',
+  templateUrl: './change-station.component.html',
+  styleUrls: ['./change-station.component.css']
+})
+export class ChangeStationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- WebApp/PUSGSAngular/src/app/edit-stations/edit-stations.component.ts
+++ WebApp/PUSGSAngular/src/app/edit-stations/edit-stations.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+export type FormType = 'AddStation' | 'ChangeStation' | 'DeleteStation';
+
 @Component({
   selector: 'app-edit-stations',
   templateUrl: './edit-stations.component.html',
@@ -8,6 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class EditStationsComponent implements OnInit {
 
   constructor() { }
+
+  form : FormType = 'AddStation';
+  
+    get showAdd()
+    {
+      return this.form === 'AddStation';
+    }
+  
+    get showChange()
+    {
+      return this.form === 'ChangeStation';
+    }
+  
+    get showDelete()
+    {
+      return this.form === 'DeleteStation';
+    }
+  
+    toggleForm(type: FormType) {
+      this.form = type;
+    }
 
   ngOnInit() {
   }
