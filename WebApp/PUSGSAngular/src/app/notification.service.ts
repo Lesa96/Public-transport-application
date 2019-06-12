9973d2b862678957c10cbcfcef265ac159b99f0f
Stefan Milenković <milenko2204@gmail.com>
2019-06-12 17:13:56 +0200

    sockets started

---
+++ WebApp/PUSGSAngular/src/app/notification.service.ts
@@ -0,0 +1,82 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+
+declare var $;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  private proxy: any;  
+  private proxyName: string = 'notifications';  
+  private connection: any;  
+  public connectionExists: Boolean; 
+
+  public notificationReceived: EventEmitter < string >;  
+
+  constructor() {  
+      this.notificationReceived = new EventEmitter<string>();
+      this.connectionExists = false;  
+      // create a hub connection  
+      this.connection = $.hubConnection("http://localhost:8080/");
+      this.connection.qs = { "token" : "Bearer " + localStorage.jwt };
+      // create new proxy with the given name 
+      this.proxy = this.connection.createHubProxy(this.proxyName);  
+      
+  }  
+ 
+  // browser console will display whether the connection was successful    
+  public startConnection(): Observable<Boolean> { 
+      
+    return Observable.create((observer) => {
+       
+        this.connection.start()
+        .done((data: any) => {  
+            console.log('Now connected ' + data.transport.name + ', connection ID= ' + data.id)
+            this.connectionExists = true;
+
+            observer.next(true);
+            observer.complete();
+        })
+        .fail((error: any) => {  
+            console.log('Could not connect ' + error);
+            this.connectionExists = false;
+
+            observer.next(false);
+            observer.complete(); 
+        });  
+      });
+  }
+
+  // public registerForClickEvents(): void {  
+      
+  //     this.proxy.on('userClicked', (data: string) => {  
+  //         console.log('received notification: ' + data);  
+  //         this.notificationReceived.emit(data);  
+  //     }); 
+  // }  
+
+  public registerForTimerEvents() : Observable<string> {
+      
+    return Observable.create((observer) => {
+        // TODO: setVehiclePosition
+        // this.proxy.on('setVehiclePosition', (data: string) => {
+        //   observer.next(data);
+        // });
+
+        this.proxy.on('setRealTime', (data: string) => {  
+            console.log('received time: ' + data);  
+            observer.next(data);
+        });  
+    });      
+  }
+
+  public StopTimer() {
+      this.proxy.invoke("StopTimeServerUpdates");
+  }
+
+  public StartTimer() {
+      this.proxy.invoke("TimeServerUpdates");
+  }
+}
